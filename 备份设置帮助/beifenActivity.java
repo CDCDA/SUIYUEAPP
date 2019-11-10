9adac001734504f7829420bf035d83c1fbc66bae
liuxinyao0722 <55103027+liuxinyao0722@users.noreply.github.com>
2019-11-10 16:40:27 +0800

    备份设置

---
+++ 备份设置帮助/beifenActivity.java
@@ -0,0 +1,14 @@
+package com.example.suiyue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class beifenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beifen);
+    }
+}
